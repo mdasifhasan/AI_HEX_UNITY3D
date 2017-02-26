ac1e208048557a902313552664db2864fae3a250
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2017-02-25 20:30:11 -0600

    Implement GameOver check

--- Assets/HexGame/GameController.cs
+++ Assets/HexGame/GameController.cs
@@ -25,6 +25,8 @@ public class GameController : MonoBehaviour
             p.OnPlayFinished += OnPlayFinished;
             p.MyTurnID = i;
         }
+
+        CheckGameOver();
         StartCoroutine(StartGame());
     }
 
@@ -47,6 +49,12 @@ public class GameController : MonoBehaviour
     private void OnPlayFinished(TileState ts)
     {
         ts.setTileState(this.currentTurn);
+        int go = CheckGameOver();
+        if (go != -1)
+        {
+            Debug.Log("GameOver: " + go);
+            return;
+        }
         tileStates.Remove(ts);
         this.currentTurn = (this.currentTurn + 1) % 2;
         CubeIndex index = ts.GetComponent<Tile>().index;
@@ -60,23 +68,125 @@ public class GameController : MonoBehaviour
         //        t.GetComponent<TileState>().setTileState(2);
         //}
 
-        while (true)
-        {
-            Hex n = Hex.Neighbor(new Hex(index.x, index.y, index.z), testDirection);
-            //Debug.Log(i + " Neightbour: " + n.q + ", " + n.r + ", " + n.s);
-            Tile t = grid.TileAt(n.q, n.r, n.s);
-            //Debug.Log("Tile found: " + t, t);
-            if (t != null)
-                t.GetComponent<TileState>().setTileState(2);
-            else
-                break;
-            index = t.index;
-        }
+        //while (true)
+        //{
+        //    Hex n = Hex.Neighbor(new Hex(index.x, index.y, index.z), testDirection);
+        //    //Debug.Log(i + " Neightbour: " + n.q + ", " + n.r + ", " + n.s);
+        //    Tile t = grid.TileAt(n.q, n.r, n.s);
+        //    //Debug.Log("Tile found: " + t, t);
+        //    if (t != null)
+        //        t.GetComponent<TileState>().setTileState(2);
+        //    else
+        //        break;
+        //    index = t.index;
+        //}
 
 
         // this is necessary to switch to next turn after each turn is finished
         isCurrentTurnUpdated = true;
 
+    }
+
+    private int CheckGameOver()
+    {
+        //Debug.Log("CheckGameOver()");
+        var lineStart = FractionalHex.HexLinedraw(new Hex(0, 0, 0), new Hex(7, 0, -7));
+        var lineGoal = FractionalHex.HexLinedraw(new Hex(0, 7, -7), new Hex(7, 7, -14));
+        if (CheckPlayerWin(0, lineStart, lineGoal))
+            return 0;
+        else
+        {
+            lineStart = FractionalHex.HexLinedraw(new Hex(0, 0, 0), new Hex(0, 7, -7));
+            lineGoal = FractionalHex.HexLinedraw(new Hex(7, 0, -7), new Hex(7, 7, -14));
+            if (CheckPlayerWin(1, lineStart, lineGoal))
+                return 1;
+        }
+        return -1;
+
+    }
+
+    private bool CheckPlayerWin(int playerID, List<Hex> lineStart, List<Hex> lineGoal)
+    {
+        //Debug.Log("CheckPlayerWin: " + playerID);
+        //foreach (Tile t in grid.Tiles.Values)
+        //{
+        //    TileState ts = t.GetComponent<TileState>();
+        //    ts.resetHighLight();
+        //}
+        List<TileState> frontier = new List<TileState>();
+        List<TileState> goals = new List<TileState>();
+        foreach (Hex n in lineStart)
+        {
+            Tile t = grid.TileAt(n.q, n.r, n.s);
+            if (t != null)
+            {
+                TileState ts = t.GetComponent<TileState>();
+                //ts.setTileState(2);
+                if (ts.currentState == playerID)
+                {
+                    frontier.Add(ts);
+                }
+            }
+        }
+
+        foreach (Hex n in lineGoal)
+        {
+            Tile t = grid.TileAt(n.q, n.r, n.s);
+            if (t != null)
+            {
+                TileState ts = t.GetComponent<TileState>();
+                //ts.setTileState(2);
+                if (ts.currentState == playerID)
+                {
+                    goals.Add(ts);
+                }
+            }
+        }
+
+        List<TileState> expanded = new List<TileState>();
+        int c = 0;
+        while (frontier.Count > 0 )
+        {
+            c++;
+            if( c > 1000)
+            {
+                Debug.LogError("Checking game over exceeding time limit");
+                break;
+            }
+            TileState ts = frontier[0];
+            frontier.RemoveAt(0);
+            expanded.Add(ts);
+            List<Tile> n = grid.Neighbours(ts.tile);
+            foreach (Tile t in n)
+            {
+                TileState nts = t.GetComponent<TileState>();
+                if (nts.currentState == playerID)
+                {
+                    if (!expanded.Contains(nts)) { 
+                        //nts.highlight();
+                        if (goals.Contains(nts))
+                        {
+                            return true;
+                        }
+                        frontier.Add(nts);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public void recurseCheckGameOver(TileState ts, int playerID)
+    {
+        int winner = -1;
+        List<Tile> n = grid.Neighbours(ts.tile);
+        List<TileState> frontier = new List<TileState>();
+        foreach(Tile t in n)
+        {
+            TileState nts = t.GetComponent<TileState>();
+            if (nts.currentState == playerID)
+                frontier.Add(nts);
+        }
     }
 
     private void UpdateCurrentPlayerText()
--- Assets/HexGame/TileState.cs
+++ Assets/HexGame/TileState.cs
@@ -6,11 +6,14 @@ public class TileState : MonoBehaviour {
     public Color colorBlack = new Color(0, 0, 0, 1);
     public Color colorWhite = new Color(1, 1, 1, 1);
     public Color colorHighlight = new Color(1, 1, 0, 1);
+    public Color colorLine;
     public int currentState = -1;
+    public Tile tile;
     // Use this for initialization
     void Start () {
-		
-	}
+        this.tile = GetComponent<Tile>();
+        colorLine = GetComponent<LineRenderer>().material.color;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,8 +28,17 @@ public class TileState : MonoBehaviour {
         else if (state == 1)
             rend.material.color = colorBlack;
         else if (state == 2)
-            rend.material.color = colorHighlight;
+            GetComponent<LineRenderer>().material.color = colorHighlight;
         if(state == 1 || state == 0)
             this.currentState = state;
+    }
+
+    public void highlight()
+    {
+        GetComponent<LineRenderer>().material.color = colorHighlight;
+    }
+    public void resetHighLight()
+    {
+        GetComponent<LineRenderer>().material.color = colorLine;
     }
 }
